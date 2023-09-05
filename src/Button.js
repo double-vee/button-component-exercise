eb7d9d77b50d8471b717be11dffa5ad0ae38d018
Weronika Kansy <weronika.kansy@gmail.com>
2023-09-06 01:23:15 +0200

    Delete or update initial files

---
+++ src/Button.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/macro';
+import { COLORS } from './constants';
+
+const Button = ({ variant, size, children }) => {
+  return 'TODO';
+};
+
+export default Button;
